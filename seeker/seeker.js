8fe48890fa3be7c023f3e6959583889ca90b25f9
kunal2144 <jkunal2144@gmail.com>
2023-03-27 11:26:32 +0530

    Seeker layout change + recruit

--- seeker/seeker.js
+++ seeker/seeker.js
@@ -1,25 +1,27 @@
 async function getJobListings() {
-    let jobListings = (await axios.get('http://127.0.0.1:3000/get_job_listings')).data
-    let feed = document.getElementById('feed')
+    let jobListings = (
+        await axios.get("http://192.168.0.189:3000/get_job_listings")
+    ).data
+    let feed = document.getElementById("feed")
 
-    let jobListingElements = jobListings.map(job => {
-        let jobListing = document.createElement('div')
-        jobListing.classList.add('job-listing')
+    let jobListingElements = jobListings.map((job) => {
+        let jobListing = document.createElement("div")
+        jobListing.classList.add("job-listing")
         jobListing.innerHTML = `
             <h1>${job.title}</h1>
             <h3>${job.description}</h3>
             <h3>${"Location: ".concat(job.location)}</h3>
             <hr>
             <h4>Qualifications: </h4>
-            <p>${job.qualifications}</p>
-            <h4 style=" color: ${(job.status == "Open")? "green" : "red"};">
-                ${(job.status == "Open")? "Active" : "Closed"}
+            <p>${job.qualifications.replaceAll(";", "</br>").toString()}</p>
+            <h4 style=" color: ${job.status == "Open" ? "green" : "red"};">
+                ${job.status == "Open" ? "Active" : "Closed"}
             </h4>
         `
         return jobListing
     })
 
-    jobListingElements.forEach(jobListingElement => {
+    jobListingElements.forEach((jobListingElement) => {
         feed.appendChild(jobListingElement)
     })
 }
